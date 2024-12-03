2f9817ca576a862fe4cb25700811c0b93981ea04
Jack <kitty2021tyj@gmail.com>
2024-12-04 07:41:27 +0800

    ✨ feat(user):新增创建用户接口

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,19 @@
 import UserService from "../service/user.service";
 
 class UserController {
-    create(ctx, next) {
+    async create(ctx, next) {
         // 1. 获取客户端传递过来的用户信息
         const user = ctx.request.body
 
         // 2. 将用户信息保存到数据库
-        UserService.create(user)
+        const result = await UserService.create(user)
 
         // 3. 返回响应
-        ctx.body = '创建用户成功~'
+        ctx.body = {
+            code: 0,
+            message: '用户注册成功~',
+            data: result
+        }
     }
 }
 
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,8 +1,15 @@
 import connection from "../app/database";
 
 class UserService {
-    create(user) {
-        connection.execute('INSERT INTO user SET ?', [user]);
+    async create(user) {
+        const { name, password } = user
+
+        // 预处理 SQL 语句
+        const statement = 'INSERT INTO user (name, password) VALUES (?, ?)'
+
+        // 执行 SQL 语句
+        const [ result ] = await connection.execute(statement, [name, password]);
+        return result
     }
 }
 
