66f85dcc18549640b07b6b21628c5456da5b0e8b
Jack <kitty2021tyj@gmail.com>
2024-12-04 07:46:08 +0800

    ✨ feat:新增根据用户名查找用户

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,6 +1,7 @@
 import connection from "../app/database";
 
 class UserService {
+    // 创建一个用户
     async create(user) {
         const { name, password } = user
 
@@ -10,6 +11,13 @@ class UserService {
         // 执行 SQL 语句
         const [ result ] = await connection.execute(statement, [name, password]);
         return result
+    }
+    
+    // 根据用户名查找用户
+    async findUserByName(name) {
+        const statement = 'SELECT * FROM user WHERE name = ?'
+       const [ values ] = await connection.execute(statement, [name])
+       return values
     }
 }
 
