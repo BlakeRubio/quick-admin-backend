6243c18efc21a0b3814f38484ecbd622075ef6da
Jack <kitty2021tyj@gmail.com>
2024-12-03 22:28:28 +0800

    🎉 init:update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import Koa from 'koa'
 // 实例化 Koa
 const app = new Koa()
 
+// 测试服务是否开启
+app.use((ctx) => {
+    ctx.body = 'Hello World!'
+})
+
 // 开启 Koa 服务
 app.listen(3000, () => {
     console.log('quick-admin-backend is running on port 3000~');
