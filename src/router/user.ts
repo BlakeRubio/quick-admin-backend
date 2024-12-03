2e46f0063a4a712b86f6a765b633a96e10801568
Jack <kitty2021tyj@gmail.com>
2024-12-04 05:41:55 +0800

    ✨ feat(user):抽离接口 controller 层和 service 层

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,14 +1,14 @@
 /** 必须使用 COMMONJS 导入,否则会有错误 */
 const KoaRouter = require('@koa/router')
+import userController from "../controller/user.controller";
+
 
 // 路由实例测试
 const userRoute = new KoaRouter({
     prefix: '/user'
 });
-userRoute.get('/info', (ctx) => {
-    ctx.body = {
-        userInfo: 'user info'
-    }
-})
+
+// 定义用户注册接口
+userRoute.post('/', userController.create)
 
 export default userRoute
---
+++ src/service/user.service.ts
@@ -0,0 +1,9 @@
+class UserService {
+    create(user) {
+        console.log('保存 user 到数据库中');
+        // 保存 user 到数据库中
+        // return user;
+    }
+}
+
+export default new UserService();
