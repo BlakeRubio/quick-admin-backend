a23214b660da1f5dd62e5a4af3847254e0bcfbf3
Jack <kitty2021tyj@gmail.com>
2024-12-04 08:19:02 +0800

    ✨ feat:新增注册接口

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,11 +1,36 @@
-import UserSchema from '../validators/user';
+import UserSchema from "../validators/user";
+import userService from "../service/user.service";
 
+interface User {
+  id: number;
+  name: string;
+  password: string;
+  createAt: Date;
+  updateAt: Date;
+}
+
+// 用户注册中间件
 const verifyUser = async (ctx, next) => {
   const user = ctx.request.body;
   const { error, value } = UserSchema.validate(user);
-  const details = error?.details ? error.details : value
-  console.log(details);
-  // next();
+  if (error) {
+    ctx.body = {
+      code: -1001,
+      message: error.details[0].message,
+    };
+    return
+  }
+
+  const hasUser = (await userService.findUserByName(user.name)) as Array<User>;
+    if (hasUser.length) {
+        ctx.body = {
+            code: -1002,
+            message: '用户名已经被占用，换一个试试吧~'
+        }
+        return
+    }
+
+    await next();
 };
 
 export default verifyUser;
