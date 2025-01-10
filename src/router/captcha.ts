017f1d8d5b77aa93ee427afb2124dee81f92e8d6
Jack <kitty2021tyj@gmail.com>
2025-01-10 11:21:28 +0800

    🎈 perf:拆分生成验证码逻辑

--- src/router/captcha.ts
+++ src/router/captcha.ts
@@ -1,22 +1,11 @@
+import { createCaptcha } from "../middlewares/captcha.middleware";
+
 const KoaRouter = require('@koa/router')
-const svgCaptcha = require('svg-captcha');
 const captchaRouter = new KoaRouter({
     prefix: '/captcha'
 })
 
-// 生成登录验证码
-captchaRouter.get('/', (ctx) => {
-    const captcha = svgCaptcha.create();
-    const base64Image = `data:image/svg+xml;base64,${Buffer.from(captcha.data).toString('base64')}`
-    ctx.body = {
-        code: 0,
-        text: captcha.text,
-        message: "生成验证码成功~", 
-        data: base64Image
-    }
-
-    // 把验证码保存到全局对象里
-    global.captcha = captcha.text
-})
+// 生成验证码
+captchaRouter.get('/', createCaptcha)
 
 export default captchaRouter
